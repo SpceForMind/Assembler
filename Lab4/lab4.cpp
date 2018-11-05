5235fa6626adbd5588de41563b72074acd17d8cf
SpceForMind <31548759+SpceForMind@users.noreply.github.com>
2018-11-05 22:35:31 +0300

    Update lab4.cpp

--- Lab4/lab4.cpp
+++ Lab4/lab4.cpp
@@ -1,11 +1,14 @@
-
+#include "pch.h"
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	ofstream out("output.txt");
 	char *strin = new char[80];
 	cout << "|--------------------|----------------------------------------------|\n"
@@ -15,50 +18,54 @@ int main()
 		"| Автор              | Давыдов А.А., группа 7304                    |\n"
 		"|--------------------|----------------------------------------------|\n\n";
 	cout << "Введите строку\n";
-	if(!cin.getline(strin, 80))
+	if (!cin.getline(strin, 80))
 	{
 		cout << "Введено больше 80 символов" << endl;
 		return 0;
 	}
 	char *strout = new char[strlen(strin) + 1];
-__asm
+	__asm
 	{
 		mov esi, strin
 		mov edi, strout
-		
-		start:
-			lodsb	// загрузка из esi в al
+
+		start :
+		lodsb	// загрузка из esi в al
 			test al, al	// сравнение al c '\0'
-			je exit
-			cmp al, 'A'	//сравнение с латинской A
-			jle start	//считывание нового символа
-			cmp al, 'Z'
-			jg check_low_latin
-				stosb	//сохранение строковых данных из al в es:edi
-				jmp start
-			check_low_latin:
-				cmp al, 'a'
-				jl start
-				cmp al, 'z'
-				jg check_rus_up
-				stosb
-				jmp start
-			check_rus_up:
-				cmp al, 'А'
-				jl start
-				cmp al, 'Я'
-				jg check_rus_low
-				stosb
-				jmp start
+			je exit1
+			cmp al, ' '
+				je wrt
+			cmp al, 'А'
+			jl start
+			cmp al, 'Я'
+			jg check_rus_low
+			jmp wrt
 			check_rus_low:
 				cmp al, 'а'
 				jl start
 				cmp al, 'я'
+				jg check_latin_up
+				jmp wrt
+			check_latin_up:
+				cmp al, 'A'	//сравнение с латинской A
+				jl start	//считывание нового символа
+				cmp al, 'Z'
+				jg check_low_latin
+				jmp wrt
+			check_low_latin :
+				cmp al, 'a'
+				jl start
+				cmp al, 'z'
 				jg start
-				stosb
-				jmp start
-		exit:
+				//next instruction is wrt
+		wrt:
+			stosb //сохранение строковых данных из al в es:edi
+			jmp start
+		exit1:
 			stosb	//сохранение строковых данных в edi 
 	}
+	cout << strout;
+	out << strout;
+	out.close();
 	return 0;
 }
